cb69adea74742134abf271fc03d2a3b0f6ece9b7
KavyaKrishnamurthy20 <kavyak222@gmail.com>
2018-04-14 12:12:50 +0000

    final finale

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+},
+        {
+            'sender': 'BOT',
+            'text': 'Hiii'
+},
+        {
+            'sender': 'USER',
+            'text': 'Whats up'
+},
+        {
+            'sender': 'BOT',
+            'text': 'Nothing much. you tell?'
+}
+];
 });
