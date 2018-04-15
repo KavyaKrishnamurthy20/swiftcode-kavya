81382fbf0cfae14b3732fd8ddc1d6f7ce8d97219
KavyaKrishnamurthy20 <kavyak222@gmail.com>
2018-04-15 06:02:39 +0000

    actors

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -6,15 +6,44 @@ import akka.actor.UntypedActor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data.FeedResponse;
 import data.Message;
+import data.NewsAgentResponse;
 import services.FeedService;
 import services.NewsAgentService;
 
 import java.util.UUID;
 
 public class MessageActor extends UntypedActor {
+    //Self reference the actor
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    private final ActorRef out;
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
+
+    private FeedService feedService = new FeedService();
+    private NewsAgentService newsAgentService = new NewsAgentService();
 
     @Override
     public void onReceive(Object message) throws Throwable {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Message messageObject = new Message();
+        if (message instanceof String) {
+
+            messageObject.text = (String) message;
+            messageObject.sender = Message.Sender.USER;
+            out.tell(objectMapper.writeValueAsString(messageObject), self());
+            newsAgentService.getNewsAgentResponse(messageObject.text, UUID.randomUUID());
+            String query = newsAgentService.getNewsAgentResponse("Find" + message, UUID.randomUUID()).query;
+            FeedResponse feedResponse = feedService.getFeedQuery(query);
+            messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
+            messageObject.feedResponse = feedResponse;
+            messageObject.sender = Message.Sender.BOT;
+            out.tell(objectMapper.writeValueAsString(messageObject), self());
+        }
 
     }
 }
